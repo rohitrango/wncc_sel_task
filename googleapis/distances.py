4a2206f263b96368ed33b9be27aa3bee42484ce1
rohitrango <rohit.rango@gmail.com>
2016-04-02 19:58:19 +0530

    GoogleAPI script working and tested

--- googleapis/distances.py
+++ googleapis/distances.py
@@ -1,3 +1,9 @@
+"""
+This script takes a file input.txt for all destination values. Takes the input from the user(via terminal). Gives a sorted list of nearest locations.
+User is prompted to enter the output file name in .txt format as well.
+Bird's eye view will be implemented soon.
+Please wait for sometime, requests may take time to download.
+"""
 import sys,requests,json
 
 class Places:
@@ -10,8 +16,9 @@ class Places:
 		return self.value
 
 	#value will be -1 if too far, -2 if not found
+	#this class will hold the name of the place, the distance and its numerical value
 
-myfile = open("input.txt")
+myfile = open("input.txt")							#taking all the destination values
 mylocations = myfile.readlines()
 myfile.close()
 
@@ -29,17 +36,17 @@ for i in range(len(mylocations)):
 
 # Checking whether source place exists or not
 
-link = "https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s"
+link = "http://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s"
 sourceData = requests.get(link%(parseSource,parseSource))
 sourceJsonData = json.loads(sourceData.text)
 
-if(sourceJsonData['status'] == "NOT_FOUND"):
+if(sourceJsonData['status'] == "NOT_FOUND"):			#will exit if the source place doesn't exist
 	print "Source value is incorrect.\n"
 	sys.exit()
 
 else:												# source place exists
 	places = []
-
+	max_value = -3									#to print the Not Found and no routes at the end. (after all nearest locations are the priority)
 	for i in range(len(parseLocations)):
 		sourceData = requests.get(link%(parseSource,parseLocations[i]))
 		sourceJsonData = json.loads(sourceData.text)
@@ -47,7 +54,7 @@ else:												# source place exists
 		destStatus = sourceJsonData['status']
 
 		if(destStatus=="NOT_FOUND"):
-			temp = Places(mylocations[i],"Not Found",-2)
+			temp = Places(mylocations[i],"Location not found",-2)
 
 		elif(destStatus=="ZERO_RESULTS"):
 			temp = Places(mylocations[i],"No driving routes available", -1)
@@ -56,8 +63,14 @@ else:												# source place exists
 			distance = sourceJsonData['routes'][0]['legs'][0]['distance']['text']
 			value = sourceJsonData['routes'][0]['legs'][0]['distance']['value']
 			temp = Places(mylocations[i],distance,value)
-
+		max_value = max(temp.value,max_value)
 		places.append(temp)		#All values inserted
+
+	for i in range(len(places)):
+		if(places[i].value== -2):
+			places[i].value = max_value + 2
+		elif(places[i].value== -1):
+			places[i].value = max_value + 1
 
 	places.sort(key=lambda Places : Places.value)		#Sort according to distance value
 
